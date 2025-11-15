70f22ae633c839766e952b8e202a84444123355c
markkashraf <mark2001rko2001@gmail.com>
2025-11-15 06:15:02 +0200

    m

--- Entities/BuyOrder.cs
+++ Entities/BuyOrder.cs
@@ -8,5 +8,16 @@ namespace Entities
 {
     internal class BuyOrder
     {
+    //        Guid BuyOrderID
+
+    //string StockSymbol[Mandatory]
+
+    //string StockName[Mandatory]
+
+    //DateTime DateAndTimeOfOrder
+
+    //uint Quantity[Value should be between 1 and 100000]
+
+    //double Price[Value should be between 1 and 10000]
     }
 }
--- Entities/SellOrder.cs
+++ Entities/SellOrder.cs
@@ -8,5 +8,16 @@ namespace Entities
 {
     internal class SellOrder
     {
+        //        Guid SellOrderID
+
+        //string StockSymbol[Mandatory]
+
+        //string StockName[Mandatory]
+
+        //DateTime DateAndTimeOfOrder
+
+        //uint Quantity[Range(1, 100000)]
+
+        //double Price[Range(1, 10000)]
     }
 }
