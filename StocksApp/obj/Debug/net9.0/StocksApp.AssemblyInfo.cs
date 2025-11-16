d882fbe6fb9bf533018d6e0767d40aee33c44ed1
markkashraf <mark2001rko2001@gmail.com>
2025-11-16 05:47:18 +0200

    added tag helpers to both of the views

--- StocksApp/obj/Debug/net9.0/StocksApp.AssemblyInfo.cs
+++ StocksApp/obj/Debug/net9.0/StocksApp.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StocksApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+70f22ae633c839766e952b8e202a84444123355c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+346f22d342ee1a4123404d88d93a1ce4deac6596")]
 [assembly: System.Reflection.AssemblyProductAttribute("StocksApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StocksApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
